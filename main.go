63d773a3655af42189d9b420f5a425248db49b9a
Casey Allred <sbditto85@gmail.com>
2014-04-09 22:45:48 -0600

    go fmt

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"os"
 	an "github.com/sbditto85/compiler/analyzer"
 	lex "github.com/sbditto85/compiler/lexer"
 	tc "github.com/sbditto85/compiler/tcode"
 	tok "github.com/sbditto85/compiler/token"
 	amb "github.com/sbditto85/virtualmachine/assembler"
 	vm "github.com/sbditto85/virtualmachine/virtualmachine"
+	"os"
 )
 
 func main() {
@@ -17,22 +17,22 @@ func main() {
 			fmt.Println(r)
 		}
 	}()
-	if len(os.Args) <=1 {
+	if len(os.Args) <= 1 {
 		fmt.Printf("Usage: compiler file.kxi [file.kxi ...]")
 		return
 	}
-	for _, file := range(os.Args[1:]) {
+	for _, file := range os.Args[1:] {
 		l := lex.NewLexer()
 		l.ReadFile(file)
-		
+
 		a := an.NewAnalyzer(l, false)
 		a.GetNext(false)
 		err := a.PerformPass()
-		
+
 		if err != nil {
 			fmt.Println(err.Error())
 		}
-		
+
 		curTok, err := l.GetCurrentToken()
 		if curTok.Type != tok.EOT {
 			panic(fmt.Sprintf("Last token not EOT it is %s\n", curTok.Lexeme))
@@ -40,16 +40,16 @@ func main() {
 		if err != nil {
 			panic("Error getting last token!")
 		}
-		
+
 		l = lex.NewLexer()
 		l.ReadFile(file)
 		a.SetLexer(l)
-		
+
 		err = a.PerformNextPass(false)
 		if err != nil {
 			panic(err.Error())
 		}
-		
+
 		curTok, err = l.GetCurrentToken()
 		if curTok.Type != tok.EOT {
 			panic(fmt.Sprintf("Last token not EOT it is %s\n", curTok.Lexeme))
@@ -57,25 +57,26 @@ func main() {
 		if err != nil {
 			panic("Error getting last token!")
 		}
-		
+
 		table, symbolTable := a.GetICodeInfo()
-		
+
 		asm := tc.GenerateASM(table, symbolTable)
-		
 
 		fmt.Printf("Enter asm file name for %s: ", file)
 		asmFileName := ""
 		fmt.Scanf("%s", &asmFileName)
 
-		if asmFileName != "" {			
+		if asmFileName != "" {
 			asmFile, err := os.Create(asmFileName)
-			if err != nil { panic(err)  }
+			if err != nil {
+				panic(err)
+			}
 			defer func() {
 				if err := asmFile.Close(); err != nil {
 					panic(err)
 				}
 			}()
-			for _, line := range(asm) {
+			for _, line := range asm {
 				if _, err := asmFile.WriteString(line + "\n"); err != nil {
 					panic(err)
 				}
@@ -84,28 +85,28 @@ func main() {
 
 		fmt.Printf("Would you like to run the assembly? (y/n): ")
 		yN := ""
-		fmt.Scanf("%s",&yN)
+		fmt.Scanf("%s", &yN)
 
 		switch yN {
-		case "y","Y":
+		case "y", "Y":
 			assembler := amb.NewAssembler()
 			assembler.ReadStrings(asm)
-			
+
 			fperr := assembler.FirstPass()
 			if fperr == nil {
 				sperr := assembler.SecondPass()
 				if sperr != nil {
-				panic(sperr.Error())
+					panic(sperr.Error())
 				}
 			} else {
 				panic(fperr.Error())
 			}
-			
+
 			v := vm.NewVirtualMachine(assembler.GetBytes())
 			verr := v.Run()
 			if verr != nil {
-				panic( verr.Error())
+				panic(verr.Error())
 			}
-		}	
+		}
 	}
 }
